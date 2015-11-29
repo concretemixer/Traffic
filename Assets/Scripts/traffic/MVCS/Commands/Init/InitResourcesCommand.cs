62265f58fc862d986bd22156804183ae2cfb945d
Aleksei Malorossiianov <funnycrabs@gmail.com>
2015-11-29 15:41:36 +0600

    wip: делаю мэнеджер ресурсов + пример UI

---
+++ Assets/Scripts/traffic/MVCS/Commands/Init/InitResourcesCommand.cs
@@ -0,0 +1,12 @@
+using strange.extensions.command.impl;
+
+namespace Traffic.MVCS.Commands.Init
+{
+	public class InitResourcesCommand : Command 
+	{
+		public override void Execute()
+		{
+			
+		}
+	}
+}
--- Assets/Scripts/traffic/MVCS/Commands/Init/InitUICommand.cs
+++ Assets/Scripts/traffic/MVCS/Commands/Init/InitUICommand.cs
@@ -1,13 +1,27 @@
+using Commons.UI;
 using Commons.Utils;
 using strange.extensions.command.impl;
+using strange.extensions.injector.api;
+using Traffic.Components;
+using UnityEngine;
 
 namespace Traffic.MVCS.Commands.Init
 {
-	public class InitUICommand : Command
-	{
-		public override void Execute()
-		{
-			Logger.Log("init UI");
-		}
-	}
+    public class InitUICommand : Command
+    {
+        [Inject]
+        public IInjectionBinder binder { private get; set; }
+
+        [Inject(EntryPoint.Container.UI)]
+        public GameObject uiContainer { private get; set; }
+
+        public override void Execute()
+        {
+            Logger.Log("init UI");
+
+            var manager = new UIManager();
+            manager.Init(uiContainer);
+            binder.Bind<IUIManager>().ToValue(manager);
+        }
+    }
 }
