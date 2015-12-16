e6b619295bf179b577126d660514a0df267fcd98
Max Pervukhin <max.pervukhin@gmail.com>
2015-12-16 13:08:15 +0600

    json

---
+++ Assets/Dependencies/JsonFx.Json/ICustomConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JsonFx.Json
+{
+    public interface ICustomConverter
+    {
+        object TryConvert(object value, Type targetType);
+    }
+}
