a61bd485ffc1310f7e8c3d0feabcddeaee03936d
Aleksei Malorossiianov <funnycrabs@gmail.com>
2016-01-08 13:40:47 +0600

    добавил префиксы в логгер для более удобной отладки на девайсе

--- Assets/Scripts/Commons/Utils/Loggr.cs
+++ Assets/Scripts/Commons/Utils/Loggr.cs
@@ -1,9 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Commons.Utils
 {
     public class Loggr
     {
+        static readonly string COMMON_PREFIX = "Loggr:";
+        static readonly string ERROR_PREFIX = "Error:";
+        static readonly string MESSAGE_PREFIX = "Message:";
+        static readonly string WARNING_PREFIX = "Warning:";
         static Loggr instance;
         static Loggr()
         {
@@ -12,13 +18,40 @@ namespace Commons.Utils
 
         public static void Log(params string[] _messages)
         {
-            instance.Print(_messages);
+            instance.Print(_messages, MESSAGE_PREFIX);
         }
 
-        void Print(string[] messages)
+        public static void Error(params string[] _messages)
         {
-            var output = string.Join(" ", messages);
+            instance.Print(_messages, ERROR_PREFIX);
+        }
+
+        int stackLength = 1000;
+        Stack<string> _logDump = new Stack<string>();
+
+        string[] addPrefix(string[] _messages, string prefix)
+        {
+            var msgs = _messages.ToList();
+            msgs.Insert(0, prefix);
+            msgs.Insert(0, COMMON_PREFIX);
+            return msgs.ToArray();
+        }
+
+        void Print(string[] _messages, string prefix = "")
+        {
+            _messages = addPrefix(_messages, prefix);
+            var output = string.Join(" ", _messages);
+
+            addToDump(output);
             Debug.Log(output);
+        }
+
+        void addToDump(string _message)
+        {
+            while (_logDump.Count > stackLength)
+                _logDump.Pop();
+
+            _logDump.Push(_message);
         }
     }
 }
