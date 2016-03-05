21879dfc3c3eea3cf0eaaa1e380ab6cebf06f315
Aleksei Malorossiianov <funnycrabs@gmail.com>
2016-03-06 00:40:36 +0600

    add crash report logger + unity ads packages

--- Assets/Scripts/traffic/Components/EntryPoint.cs
+++ Assets/Scripts/traffic/Components/EntryPoint.cs
@@ -1,6 +1,7 @@
 ﻿using strange.extensions.context.impl;
 using Traffic.MVCS;
 using UnityEngine;
+using UnityEngine.CrashLog;
 
 namespace Traffic.Components
 {
@@ -8,44 +9,34 @@ namespace Traffic.Components
     {
         public static string DebugMessage = "";
 
-        [SerializeField]
-        GameObject ui;
-        [SerializeField]
-        GameObject stage;
-        [SerializeField]
-        GameObject stageMenu;
+        [SerializeField] GameObject ui;
+        [SerializeField] GameObject stage;
+        [SerializeField] GameObject stageMenu;
 
         void Start()
         {
+            CrashReporting.Init("6d41d8c6-1449-4459-b1df-45d40c474002");
             context = new AppContext(this);
             context.Start();
         }
 
         public GameObject UI
         {
-            get
-            {
-                return ui;
-            }
+            get { return ui; }
         }
 
         public GameObject Stage
         {
-            get
-            {
-                return stage;
-            }
+            get { return stage; }
         }
 
         public GameObject StageMenu
         {
-            get
-            {
-                return stageMenu;
-            }
-        }        
+            get { return stageMenu; }
+        }
 
-        public enum Container {
+        public enum Container
+        {
             StageMenu,
             Stage,
             UI
