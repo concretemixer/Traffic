ad86f32013e9222c4d00712cb899fc19c6c2f935
silvertoad <a.malorossiianov@crabscrew.ru>
2016-04-24 22:53:34 +0300

    fix appodeal init. split ios/android keys

--- Assets/Scripts/traffic/MVCS/Commands/Init/InitializeAppodealCommand.cs
+++ Assets/Scripts/traffic/MVCS/Commands/Init/InitializeAppodealCommand.cs
@@ -17,9 +17,14 @@ public class InitializeAppodealCommand : Command
 
     public override void Execute()
     {
+        #if UNITY_IOS
+        String appKey = "18e77ef8f5b7a2c7ca4a48464eb27de51c7364b9917032fe";
+        #elif UNITY_ANDROID
         String appKey = "3e28bb6cebdf9e7bba63c8bb376df09ec65dd8eea3b8ec12";
+        #endif
      //   Appodeal.setTesting(true);
     //    Appodeal.setLogging(true);
-        Appodeal.initialize(appKey, Appodeal.NON_SKIPPABLE_VIDEO);
+        Appodeal.disableLocationPermissionCheck();
+        Appodeal.initialize(appKey, Appodeal.SKIPPABLE_VIDEO);
     }
 }
