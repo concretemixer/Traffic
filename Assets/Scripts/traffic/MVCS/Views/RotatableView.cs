db92dffdb21eb06af0ac9e7ce9760d8d309efd03
concretemixer <max.pervukhin@gmail.com>
2016-05-09 02:46:50 +0600

    vertical ui fix

--- Assets/Scripts/traffic/MVCS/Views/RotatableView.cs
+++ Assets/Scripts/traffic/MVCS/Views/RotatableView.cs
@@ -9,16 +9,19 @@ namespace Traffic.MVCS.Views.UI
         public virtual void Layout(int width, int height)
         {
             bool isVertical = height > width;
+            float ratio = (float)height / (float)width;
+
             GameObject[] vert = GameObject.FindGameObjectsWithTag("UI Vertical");
             foreach (var o in vert)
             {
                 if (o.GetComponent<Image>() != null) {                    
                     o.GetComponent<Image>().enabled = isVertical;
-                    if (o.GetComponent<RectTransform>().rect.width > 599)
-                    {                      
-                        float k = width / o.GetComponent<RectTransform>().rect.width;
+                    if (ratio>1 && o.GetComponent<RectTransform>().rect.width > 599)
+                    {
+                        float k = (960 / ratio) / o.GetComponent<RectTransform>().rect.width;
                         if (k < 1)
                             k = 1;
+
                         o.GetComponent<RectTransform>().localScale = new Vector3(k,1 , 1);                        
                     }
                 }
